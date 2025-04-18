318fc362bb1b702816500ed8e36ff377d5a03036
Franco Montenegro <franco@francomontenegro.com.ar>
2025-04-18 14:45:16 -0300

    include frames and sprite ids in generated files

---
+++ example/sprites.h
@@ -0,0 +1,17 @@
+static const char *sprites_paths[] = {
+    "9foo.png",
+    "9bar.png",
+    "19baz.png",
+};
+enum sprites_id = {
+    foo,
+    bar,
+    baz,
+    sprite_count,
+};
+// x, y, w, h, frames
+static int sprites_coordinates[][5] = {
+    {0, 64, 576, 64, 9},
+    {1216, 0, 576, 64, 9},
+    {0, 0, 1216, 64, 19},
+};
--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdlib.h> // calloc
-#include <stdio.h>  // fprintf, FILE, fopen, fclose, fwrite
+#include <stdio.h>  // fprintf, FILE, fopen, fclose, fwrite, sscanf
 
 #define STBI_NO_SIMD
 
@@ -16,49 +16,56 @@ typedef unsigned char byte;
 
 int main(int argc, char **argv) {
     if (argc == 1) {
-        fprintf(stderr, 
-                "usage: %s output_name a.png b.png ...\n"
+        fprintf(stderr,
+                "usage: %s output_name 1a.png 2b.png ...\n"
+                "\n"
                 "this generates two files:\n"
-                "    output_name.png: image of 2048x2048 pixels containing a.png, b.png, etc in one file.\n"
-                "    output_name.h: contains the path, coordinates and dimensions of all images.\n",
+                "    output_name.png: image of 2048x2048 pixels containing 1a.png, 1b.png, etc in one file.\n"
+                "    output_name.h: contains the path, coordinates, dimensions and frames of all images.\n"
+                "\n"
+                "each filename must include the number of frames at the beginning. for example, "
+                "from a file named 2knight.png we can infer it has 2 frames.\n"
+                "\n",
                 argv[0]);
         return 0;
     }
-    
+
     struct state {
         stbrp_context context;
         stbrp_node nodes[2048];
         stbrp_rect rects[2048];
-        
+
         char header_name[128];
         char png_name[128];
-        
+
         byte atlas_image[2048 * 2048 * 4];
     } *state = calloc(1, sizeof(*state));
-    
-    // Initialize with width and height of 2048
+
+    // Initialize with width and height of 2048.
     stbrp_init_target(&state->context, 2048, 2048, state->nodes, 2048);
-    
+
     char *output_name = argv[1];
     snprintf(state->header_name, sizeof(state->header_name) - 1, "%s.h", output_name);
     snprintf(state->png_name, sizeof(state->png_name) - 1, "%s.png", output_name);
-    
-    // Create output header file
-    FILE *header_file = fopen(state->header_name, "w+");
-    if (!header_file) {
-        fprintf(stderr, "unable to create %s file\n", state->header_name);
-        return 0;
-    }
-    
-    // Load the images to fetch width & height
+
+    // Load the images to fetch width & height.
     int num_images = argc - 2;
     for (int i = 0; i < num_images; i++) {
         int n = 0;
-        free(stbi_load(argv[i + 2], &state->rects[i].w, &state->rects[i].h, &n, 0));
+        void *image = stbi_load(argv[i + 2], &state->rects[i].w, &state->rects[i].h, &n, 0);
+        if (!image) {
+            fprintf(stderr, "unable to load image %s.\n", argv[i + 2]);
+            return 0;
+        }
+        free(image);
     }
-    stbrp_pack_rects(&state->context, state->rects, num_images);
-    
-    // Create the output image
+
+    if (!stbrp_pack_rects(&state->context, state->rects, num_images)) {
+        fprintf(stderr, "unable to pack all pngs in output file.\n");
+        return 0;
+    }
+
+    // Create the output image.
     byte *atlas_image = state->atlas_image;
     for (int i = 0; i < num_images; i++) {
         int x = state->rects[i].x;
@@ -66,7 +73,7 @@ int main(int argc, char **argv) {
         int w = state->rects[i].w;
         int h = state->rects[i].h;
         int n = 0;
-        
+
         byte *image = stbi_load(argv[i + 2], &w, &h, &n, 0);
         for (int j = 0; j < h; j++) {
             for (int k = 0; k < w; k++) {
@@ -80,35 +87,70 @@ int main(int argc, char **argv) {
         }
         free(image);
     }
-    
-    // Save the output image
-    stbi_write_png(state->png_name, 2048, 2048, 4, atlas_image, 2048 * 4);
-    
-    // Write paths to header file
+
+    // Create output header file.
+    FILE *header_file = fopen(state->header_name, "w+");
+    if (!header_file) {
+        fprintf(stderr, "unable to create %s file.\n", state->header_name);
+        return 0;
+    }
+
+    // Save the output image.
+    if (!stbi_write_png(state->png_name, 2048, 2048, 4, atlas_image, 2048 * 4)) {
+        fprintf(stderr, "unable to write atlas png\n");
+        return 0;
+    }
+
+    // Write paths to header file.
     {
         fprintf(header_file, "static const char *%s_paths[] = {\n", output_name);
         for (int i = 0; i < num_images; i++)
             fprintf(header_file, "    \"%s\",\n", argv[i + 2]);
         fprintf(header_file, "};\n");
     }
-    
-    // Write x, y, w, h to header file
+
+    // Write ids to header file.
     {
-        fprintf(header_file, 
-                "// x, y, w, h\n"
-                "static int %s_coordinates[][4] = {\n", 
+        fprintf(header_file, "enum %s_id = {\n", output_name);
+        for (int i = 0; i < num_images; i++) {
+            int frames = 0;
+            char file_name[256] = {0};
+            // Example: 2my_picture.png, from here, we can infer it has 2 frames.
+            sscanf(argv[i + 2], "%d%s", &frames, file_name);
+            // Drop the file extension from the file name.
+            for (int n = 0; file_name[n]; n++) {
+                if (file_name[n] == '.') {
+                    file_name[n] = 0;
+                    break;
+                }
+            }
+            fprintf(header_file, "    %s,\n", file_name);
+        }
+        fprintf(header_file, "    sprite_count,\n");
+        fprintf(header_file, "};\n");
+    }
+
+    // Write x, y, w, h, frames to header file.
+    {
+        fprintf(header_file,
+                "// x, y, w, h, frames\n"
+                "static int %s_coordinates[][5] = {\n",
                 output_name);
-        for (int i = 0; i < num_images; i++)
+        for (int i = 0; i < num_images; i++) {
+            int frames = 1;
+            sscanf(argv[i + 2], "%d", &frames);
             fprintf(header_file,
-                    "    {%d, %d, %d, %d},\n", 
+                    "    {%d, %d, %d, %d, %d},\n",
                     state->rects[i].x,
                     state->rects[i].y,
                     state->rects[i].w,
-                    state->rects[i].h);
+                    state->rects[i].h,
+                    frames);
+        }
         fprintf(header_file, "};\n");
     }
-    
+
     fclose(header_file);
-    
+
     return 0;
 }
